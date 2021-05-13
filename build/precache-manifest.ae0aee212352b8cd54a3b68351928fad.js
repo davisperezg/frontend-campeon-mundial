c45dfb6f88f3ba442c0d5303ac5b803a90ae1b1d
Davis Keiner Pérez Guzmán <davisperezg@gmail.com>
2021-05-13 12:32:47 -0500

    build nuevo por botones desactivos

---
+++ build/precache-manifest.ae0aee212352b8cd54a3b68351928fad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466cf6263f4d561d6ed05985c6dd679b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ebd68231a4a39f74ea9",
+    "url": "/static/css/3.82bfcd99.chunk.css"
+  },
+  {
+    "revision": "d8d7448841b87b84d6b7",
+    "url": "/static/css/main.ee322f39.chunk.css"
+  },
+  {
+    "revision": "c60833d682ae50496368",
+    "url": "/static/js/2.9828eda1.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/2.9828eda1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebd68231a4a39f74ea9",
+    "url": "/static/js/3.2ae73b05.chunk.js"
+  },
+  {
+    "revision": "c396e98a9ac714872d33cfe88596894b",
+    "url": "/static/js/3.2ae73b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf984a390c61328c8b4c",
+    "url": "/static/js/4.2884dc58.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.2884dc58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f3af3bc6fa9952ea53",
+    "url": "/static/js/5.20a85b00.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.20a85b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d7448841b87b84d6b7",
+    "url": "/static/js/main.bf53bf3b.chunk.js"
+  },
+  {
+    "revision": "c2f6831fd2a9ebc17b52",
+    "url": "/static/js/runtime-main.a92383b6.js"
+  }
+]);
